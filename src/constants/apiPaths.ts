b11566bb911dc602f641107888d12fef963837ba
Javokhirbek Mirzaakbarov <javokhirbek_mirzaakbarov@epam.com>
2023-11-29 12:15:50 +0500

    Task-5 completed

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://79l01za1g0.execute-api.eu-north-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://456vszj4z3.execute-api.eu-west-1.amazonaws.com/dev/import",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
